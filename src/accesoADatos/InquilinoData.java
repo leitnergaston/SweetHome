3fc6da8bdb4a70c6ba88b89a69a892dbb1a304e6
Gaston <leitnergaston@gmail.com>
2023-10-03 12:57:52 -0300

    inquilinoData

--- src/accesoADatos/InquilinoData.java
+++ src/accesoADatos/InquilinoData.java
@@ -1,41 +1,34 @@
-
 package accesoADatos;
 
 import entidades.Inquilino;
 import java.sql.*;
 import javax.swing.JOptionPane;
 
-
 public class InquilinoData {
-    
+
     // Atributo tipo connection para usar sentencias sql
     private Connection con = null;
-    
+
     // Constructor
-    public InquilinoData(){
-        
+    public InquilinoData() {
+
         // Inicializar la variable con que la obtenemos de la clase Conexion y accedemos al metodo getConnection q carga los drivers
         con = Conexion.getConnection();
     }
-    
+
     // ====== METODO AGREGAR INQUILINO ======//
-    public void agregarInquilino(Inquilino inquilino){
+    public void agregarInquilino(Inquilino inquilino) {
+
         
-        // Sentencia INSERT sql
-        String sql = "INSERT INTO inquilinos (idInquilinos, cuit, nombre, apellido, lugarTrabajo, nombreGarante, dniGarante, idInmueble)"
-                + " VALUES (null, ?, ?, ?, ?, ?, ?, ?)";
-        
-        try{
-            
-            // Generar el ps para enviar las sentencias sql
-            // le pasamos la sentencia sql creada
-            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            
-            
-        }catch (SQLException ex){
-            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla inquilijno");
-        }
-                
-                
     }
+
+    //====== METODO ELIMINAR INQUILINO ======//
+    public void eliminarInquilino(int cuit) {
+
+        
+    }
+
+    //====== METODO MODIFICAR INQUILINO ======//
+    //====== METODO BUSCAR INQUILINO ======//
+    //====== METODO MULTAR INQUILINO ======//
 }
