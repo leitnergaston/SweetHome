86ea8de68439227a04b648ac78893f98622097d9
Lucas <herrscher005@gmail.com>
2023-10-04 12:51:55 -0300

    clase inquilino data

--- src/inmobiliaria32/Inmobiliaria32.java
+++ src/inmobiliaria32/Inmobiliaria32.java
@@ -2,6 +2,8 @@
 package inmobiliaria32;
 
 import accesoADatos.Conexion;
+import accesoADatos.InquilinoData;
+import entidades.Inquilino;
 import java.sql.Connection;
 
 
@@ -13,6 +15,11 @@ public class Inmobiliaria32 {
         System.out.println("El main");
         
         Connection con = Conexion.getConnection();
+        Inquilino inq = new Inquilino("pepe", "juan", 741056, "toledo", 468004, 
+                "marco", true);
+        InquilinoData inqData = new InquilinoData();
+        inqData.agregarInquilino(inq);
+        
     }
     
 }
