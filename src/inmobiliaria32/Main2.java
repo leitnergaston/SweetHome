a0bfc26f124500e99755c4163c9849e236c4988d
Lucas <herrscher005@gmail.com>
2023-10-06 11:00:32 -0300

    pruebas el main
    todos los metodos de inquilino data funcionan

--- src/inmobiliaria32/Main2.java
+++ src/inmobiliaria32/Main2.java
@@ -1,5 +1,3 @@
-
-
 package inmobiliaria32;
 
 import accesoADatos.Conexion;
@@ -12,8 +10,8 @@ import entidades.Propietario;
 import java.sql.Connection;
 import java.util.ArrayList;
 
-
 public class Main2 {
+
     public static void main(String[] args) {
         Connection con = Conexion.getConnection();
         
@@ -21,16 +19,16 @@ public class Main2 {
         InquilinoData inqData = new InquilinoData();
         InmuebleData inmData = new InmuebleData();
         
-        
         Propietario propietario = new Propietario(1, 111, "lopez", "mario", "calle 42", "2619845784", "lopezmario@mail.com", true);
         Inquilino inquilino = new Inquilino(2, "maria", "Angeles", 0011100, "ciudad", 010101, "pablo", true);
         Inmueble inmueble = new Inmueble(propietario, inquilino, "palmeras 23", "depto", 400, 80000, "centro", true);
         Inmueble inmueble2 = new Inmueble(null, null, "plaza 45", "casa", 650, 120000, "centro", true);
-        
+
         //proData.crearPropietario(propietario);
         //inqData.agregarInquilino(inquilino);
         //inmData.crearInmueble(inmueble2);
-        
+        proData.crearPropietario(propietario);
+
         /*
         
         ArrayList<Inmueble> listaInm =  inmData.listarInmueblesPorParametros("depto", 300, 10000, 100000,  "centro");
@@ -39,18 +37,10 @@ public class Main2 {
             System.out.println(inmuebleFor);
         }
         
-*/
-        
-        Inmueble inmueble3 = inmData.buscarInmueble(5);
-        
-        System.out.println(inmueble3);
-        
-        
-        
-        
-        
-        
-        
+         */
+//        Inmueble inmueble3 = inmData.buscarInmueble(5);
+//        
+//        System.out.println(inmueble3);
     }
-
+    
 }
