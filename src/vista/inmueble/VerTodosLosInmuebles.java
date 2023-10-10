3819256edffc07f8a819a78ff0c59ccb2f970f56
maty77m <ale77m@gmail.com>
2023-10-10 11:09:02 -0300

    Creación de paquetes

---
+++ src/vista/inmueble/VerTodosLosInmuebles.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista.inmueble;
+
+/**
+ *
+ * @author ale77
+ */
+public class VerTodosLosInmuebles {
+    
+}
